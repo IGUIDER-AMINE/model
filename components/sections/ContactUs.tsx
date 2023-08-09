859bcca33105dbc3734f2d7aa6841d8e3c95ddf1
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-08-09 16:48:33 +0200

    finish Contact Us

--- components/sections/ContactUs.tsx
+++ components/sections/ContactUs.tsx
@@ -1,7 +1,40 @@
+import { img1 } from "@/public/assets";
+import Image from "next/image";
 import React from "react";
+import {
+  BsInstagram,
+  BsTwitter,
+  BsFillArrowUpRightCircleFill,
+} from "react-icons/bs";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <section id="ContactUs" className="container mx-auto space-y-6 px-4">
+      <h1 className="font-titleFont text-center text-white font-bold text-4xl">
+        <span className="text-standardBlue">Connect</span> and{" "}
+        <br className="hidden md:block" />
+        Collaborate with Us
+      </h1>
+      <div className="flex gap-4 place-content-center text-white">
+        <div className="flex items-center gap-x-2">
+          <BsInstagram />
+          <span>Instagram</span>
+        </div>
+        <div className="text-standardOrange text-[20px]">+</div>
+        <div className="flex items-center gap-x-2">
+          <BsTwitter />
+          <span>Twitter</span>
+        </div>
+      </div>
+      <div>
+        <Image src={img1} alt="Models" />
+        <button>
+          <span>Contact Us</span>
+          <BsFillArrowUpRightCircleFill className="bg-white rounded-full text-standardBlack p-4" />
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default ContactUs;
---
+++ public/assets/index.ts
@@ -0,0 +1,9 @@
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img1.jpg";
+import img3 from "./images/img1.jpg";
+import img4 from "./images/img1.jpg";
+import img5 from "./images/img1.jpg";
+import img6 from "./images/img1.jpg";
+import img7 from "./images/img1.jpg";
+
+export { img1, img2, img3, img4, img5, img6, img7 };
