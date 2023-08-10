2f0304465da123fd52cda6f11f9388e65453f4af
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-08-10 10:42:17 +0200

    finish models section

--- util/data.tsx
+++ util/data.tsx
@@ -6,38 +6,38 @@ const modelsData = [
   },
   {
     image: "/assets/images/img6.jpg",
-    fullName: "Sofia Carolina",
-    information: "Russian Skinny",
+    fullName: "Oliver Cheshire",
+    information: "English Skinny",
   },
   {
     image: "/assets/images/img3.jpg",
     fullName: "Sofia Carolina",
-    information: "Russian Skinny",
+    information: "Italian Skinny",
   },
   {
     image: "/assets/images/img5.jpg",
-    fullName: "Sofia Carolina",
-    information: "Russian Skinny",
+    fullName: "Sean O'Pry",
+    information: "American Skinny",
   },
   {
     image: "/assets/images/img7.jpg",
-    fullName: "Sofia Carolina",
+    fullName: "David Gandy",
     information: "Russian Skinny",
   },
   {
     image: "/assets/images/img9.jpg",
-    fullName: "Kim Jennie",
-    information: "Russian Skinny",
+    fullName: "Kaia Jordan Gerber",
+    information: "American Skinny",
   },
   {
     image: "/assets/images/img8.jpg",
-    fullName: "Sofia Carolina",
-    information: "Russian Skinny",
+    fullName: "Armando Cabral",
+    information: "Dutch Skinny",
   },
   {
     image: "/assets/images/img4.jpg",
-    fullName: "Sofia Carolina",
-    information: "Russian Skinny",
+    fullName: "Emily Ratajkowski",
+    information: "English Skinny",
   },
 ];
 
